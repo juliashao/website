ba14e42003b960ca5be325ad18d2714953285d31
juliashao <jxs206@nyu.edu>
2015-11-23 17:26:14 -0500

    website

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-$(window).load(function() {
-	$(".button").on("mouseover", function() {
-		$(this).addClass("");
-	}); 
-
-	$(".button").off("mouseout", function(){
-		$(this).removeClass("");
-	})
+$(document).ready(function() {
+ 
+  $("#home-carousel").homeCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
 });
-
-
 
 	
