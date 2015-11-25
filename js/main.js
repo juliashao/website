56fec6edb353510519a3c3a05521c4fdc0da44f7
juliashao <jxs206@nyu.edu>
2015-11-24 19:44:41 -0500

    website

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
  
-  $("#home-carousel").homeCarousel({
+  $("#home-carousel").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 5000, //Set AutoPlay to 3 seconds
  
       items : 3,
       itemsDesktop : [1199,3],
