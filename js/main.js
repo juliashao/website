7572bb95c31420e3642c641658788b3f8c11e0bd
juliashao <jxs206@nyu.edu>
2015-12-03 18:25:54 -0500

    website

--- js/main.js
+++ js/main.js
@@ -12,4 +12,11 @@ $(document).ready(function() {
  
 });
 
-	
+$('img').hover(
+    function(){
+      $(this).css('opacity', '0.7');
+    },
+    function(){
+      $(this).css('opacity', '1');
+    }
+);
